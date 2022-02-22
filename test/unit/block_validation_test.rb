6223029fcfb6e0a844a6e8161cc4fe033d86938a
Liane Hampe <liaham@xmera.de>
2022-02-22 14:28:25 +0100

    Fix block validation tests

--- test/unit/block_validation_test.rb
+++ test/unit/block_validation_test.rb
@@ -25,111 +25,111 @@ class BlockValidationTest < RedmineDashboards::TestCase
     activity_block = find_or_create_block(ActivityBlock)
     settings = { max_entries: '5', me_only: '1' }
     activity_block.send :update_settings, settings
-    assert activity_block.valid?
+    assert activity_block.valid?, activity_block.errors.full_messages
   end
 
   def test_invalid_activity_settings
     activity_block = find_or_create_block(ActivityBlock)
     settings = { max_entries: nil, me_only: 'yes' }
     activity_block.send :update_settings, settings
-    assert activity_block.invalid?
+    assert activity_block.invalid?, activity_block.errors.full_messages
   end
 
   def test_valid_documents_settings
     documents_block = find_or_create_block(DocumentsBlock)
     settings = { max_entries: '5' }
     documents_block.send :update_settings, settings
-    assert documents_block.valid?
+    assert documents_block.valid?, documents_block.errors.full_messages
   end
 
   def test_invalid_documents_settings
     documents_block = find_or_create_block(DocumentsBlock)
     settings = { max_entries: '0' }
     documents_block.send :update_settings, settings
-    assert documents_block.invalid?
+    assert documents_block.invalid?, documents_block.errors.full_messages
   end
 
   def test_valid_feed_settings
     feed_block = find_or_create_block(FeedBlock)
     settings = { max_entries: '5', url: 'https://www.ruby-lang.org/de/feeds/news.rss' }
     feed_block.send :update_settings, settings
-    assert feed_block.valid?
+    assert feed_block.valid?, feed_block.errors.full_messages
   end
 
   def test_invalid_feed_settings
     feed_block = find_or_create_block(FeedBlock)
     settings = { max_entries: '0', url: 'https://www.ruby-lang.org/de/feeds/news.rss' }
     feed_block.send :update_settings, settings
-    assert feed_block.invalid?
+    assert feed_block.invalid?, feed_block.errors.full_messages
   end
 
   def test_valid_issue_query_settings
     issue_query_block = find_or_create_block(IssueQueryBlock)
     settings = { max_entries: '5' }
     issue_query_block.send :update_settings, settings
-    assert issue_query_block.valid?
+    assert issue_query_block.valid?, issue_query_block.errors.full_messages
   end
 
   def test_invalid_issue_query_settings
     issue_query_block = find_or_create_block(IssueQueryBlock)
     settings = { max_entries: '0' }
     issue_query_block.send :update_settings, settings
-    assert issue_query_block.invalid?
+    assert issue_query_block.invalid?, issue_query_block.errors.full_messages
   end
 
   def test_valid_my_spent_time_settings
     my_spent_time_block = find_or_create_block(MySpentTimeBlock)
     settings = { days: '5' }
     my_spent_time_block.send :update_settings, settings
-    assert my_spent_time_block.valid?
+    assert my_spent_time_block.valid?, my_spent_time_block.errors.full_messages
   end
 
   def test_invalid_my_spent_time_settings
     my_spent_time_block = find_or_create_block(MySpentTimeBlock)
     settings = { days: '0' }
     my_spent_time_block.send :update_settings, settings
-    assert my_spent_time_block.invalid?
+    assert my_spent_time_block.invalid?, my_spent_time_block.errors.full_messages
   end
 
   def test_valid_news_settings
     news_block = find_or_create_block(NewsBlock)
     settings = { max_entries: '5' }
     news_block.send :update_settings, settings
-    assert news_block.valid?
+    assert news_block.valid?, news_block.errors.full_messages
   end
 
   def test_invalid_news_settings
     news_block = find_or_create_block(NewsBlock)
     settings = { max_entries: '0' }
     news_block.send :update_settings, settings
-    assert news_block.invalid?
+    assert news_block.invalid?, news_block.errors.full_messages
   end
 
   def test_valid_text_settings
     text_block = find_or_create_block(TextAsyncBlock)
-    settings = { text: 'Lore ipsum' }
+    settings = { text: 'Lore ipsum', css: 'unset' }
     text_block.send :update_settings, settings
-    assert text_block.valid?
+    assert text_block.valid?, text_block.errors.full_messages
   end
 
   def test_invalid_text_settings
     text_block = find_or_create_block(TextAsyncBlock)
     settings = { text: '' }
     text_block.send :update_settings, settings
-    assert text_block.invalid?
+    assert text_block.invalid?, text_block.errors.full_messages
   end
 
   def test_valid_text_async_settings
     text_block = find_or_create_block(TextBlock)
-    settings = { text: 'Lore ipsum' }
+    settings = { text: 'Lore ipsum', css: 'unset' }
     text_block.send :update_settings, settings
-    assert text_block.valid?
+    assert text_block.valid?, text_block.errors.full_messages
   end
 
   def test_invalid_text_async_settings
     text_block = find_or_create_block(TextBlock)
     settings = { text: '' }
     text_block.send :update_settings, settings
-    assert text_block.invalid?
+    assert text_block.invalid?, text_block.errors.full_messages
   end
 end
